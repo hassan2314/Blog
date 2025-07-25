0d7aff34725cdefb299e408265836def56827de7
Hassan Ahmed <hassan87691@gmail.com>
2025-07-25 11:01:23 +0500

    env remove

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,16 @@
-const conf = {
+const conf2 = {
   appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
   appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
   appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
   appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
   appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+};
+const conf = {
+  appwriteUrl: "https://fra.cloud.appwrite.io/v1",
+  appwriteProjectId: "68675ddd001f50cdb66a",
+  appwriteDatabaseId: "68675edf000d769f9649",
+  appwriteCollectionId: "68675f2700181cd7494d",
+  appwriteBucketId: "6867610700217aa80d24",
 };
 
 export default conf;
